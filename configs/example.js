3fe6bbe440d016e6c302a34a394663214de5f074
Kőrösi Zoltán <korosi.zoltan@webnpro.hu>
2022-01-30 16:32:10 +0100

    Update example.js

--- configs/example.js
+++ configs/example.js
@@ -1,29 +1,12 @@
 module.exports = {
+    prefix: 'tw-',
+    separator: '_',
     theme: {
-      screens: {
-        sm: '640px',
-        md: '768px',
-        lg: '1024px',
-        xl: '1280px',
-      },
-      fontFamily: {
-        display: ['Gilroy', 'sans-serif'],
-        body: ['Graphik', 'sans-serif'],
-      },
-      borderWidth: {
-        default: '1px',
-        '0': '0',
-        '2': '2px',
-        '4': '4px',
-      },
-      extend: {
-        colors: {
-          cyan: '#9cdbff',
-        },
-        spacing: {
-          '96': '24rem',
-          '128': '32rem',
-        }
-      }
+    screens: {
+      'xl': {'max': '1119px'},
+      'lg': {'max': '991px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '479px'},
     }
   }
+}
